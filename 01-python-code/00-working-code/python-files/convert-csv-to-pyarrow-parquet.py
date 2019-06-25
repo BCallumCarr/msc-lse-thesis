28fdfdc0e81e82254aa3822b7ed6f07f27251c7c
Bcallumcarr <brad@securewealth.co.za>
2019-06-25 13:52:06 +0100

    added python function files

---
+++ 01-python-code/00-working-code/python-files/convert-csv-to-pyarrow-parquet.py
@@ -0,0 +1,12 @@
+##### load in a csv/json to try and convert to parquet #####
+import pandas as pd
+
+csv = pd.read_csv(r"initial-data/file-000000000000.json")
+print(csv.head(5))
+
+## conversion to parquet
+csv.to_parquet("initial-data/file-01.parquet") # uses pyarrow, which fixes a spark java error
+
+## test that data converted successfully
+data = pd.read_parquet('initial-data/file-01.parquet', engine='pyarrow')
+print(data.head(5))
