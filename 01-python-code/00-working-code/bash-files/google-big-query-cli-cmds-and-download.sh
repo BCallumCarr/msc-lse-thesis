c4ad3b76e791a0e4c2bd673f52f8706810612d55
Bcallumcarr <brad@securewealth.co.za>
2019-07-08 22:00:01 +0100

    deleted old working-code folder

--- 01-python-code/00-working-code/bash-files/google-big-query-cli-cmds-and-download.sh
+++
@@ -1,8 +0,0 @@
-bq show stack-exchange-project:stackoverflow_stackexchange.stackoverflow_final
-
-bq query "SELECT count(*) FROM stackoverflow_stackexchange.stackoverflow_final"
-
-bq extract stackoverflow_stackexchange.stackoverflow_final 'gs://bucket-brad-project/final-datasets/file-*.json'
-
-## copy first file to local machine
-gsutil cp gs:/bucket-brad-project/final-datasets/stackoverflow.stackexchange.com/file-000000000000.json .
--- 01-python-code/00-working-code/bash-files/my-actions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python -m pip install nltk
-python -m pip install --upgrade pip
-python -m nltk.downloader -d /usr/local/share/nltk_data all # check whether it is there.
